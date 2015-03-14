f2b44a5b23f82128eef5bc4e629a4c3a5b7f2260
RafalKlosek@gmail.com <RafalKlosek@gmail.com@da968056-76ee-959d-0430-c202c979fb66>
2015-03-14 20:47:06 +0000

    -została graficzna czesc, reszta w sumie działa :)

--- Zadanie04_weather/MainWindow.xaml.cs
+++ Zadanie04_weather/MainWindow.xaml.cs
@@ -26,27 +26,96 @@ namespace Zadanie04_weather
 	{
 		public NotifyIcon TrayIcon { get; set; }
 
+		public Timer appTimer { get; set; }
+
+		public int TimeInterval { get; set; }
+
+		public Weather appWeather { get; set; }
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			InitializeApp();
 
 			Weather testowy = new Weather("Katowice");
+			Weather testowy2 = new Weather("Dąbrowa Górnicza");
+			Weather testowy3 = new Weather("grand canyon, az");
+			testText.Text = appWeather.XMLResponse.City + " " + appWeather.XMLResponse.ConditionTemp + "C " + appWeather.XMLResponse.ConditionText + " " + appWeather.XMLResponse.Country;
 		}
 
 		public void InitializeApp()
 		{
+			InitializeTray();
+			appWeather = new Weather("katowice");
+			InitializeTimer();
+		}
+
+		private void InitializeTimer()
+		{
+			appTimer = new Timer();
+			TimeInterval = 1;
+			appTimer.Interval = TimeInterval * 60000;
+			appTimer.Tick += new EventHandler(timerHandler);
+			appTimer.Start();
+		}
+
+		private void timerHandler(object sender, EventArgs e)
+		{
+			this.appWeather.Refresh();
+			testText.Text = appWeather.XMLResponse.City + " " + appWeather.XMLResponse.ConditionTemp + "C " + appWeather.XMLResponse.ConditionText + " " + appWeather.XMLResponse.Country;
+		}
+
+		private void InitializeTray()
+		{
 			TrayIcon = new NotifyIcon();
 			TrayIcon.Icon = new Icon("Resources/main.ico");
 			TrayIcon.Visible = true;
+			TrayIcon.MouseDown += new System.Windows.Forms.MouseEventHandler(MouseDown_handler);
+			TrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(MouseDoubleClicker);
+		}
+
+		private void MouseDoubleClicker(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if ((e.Button == MouseButtons.Left) && (e.Clicks == 2))
+			{
+				this.Show();
+				if (this.WindowState == WindowState.Minimized)
+					this.WindowState = WindowState.Normal;
+			}
+		}
+
+		private void MouseDown_handler(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+				System.Windows.Controls.ContextMenu menu = (System.Windows.Controls.ContextMenu)this.FindResource("NotifierContextMenu");
+				menu.IsOpen = true;
+			}
 		}
 
 		protected override void OnStateChanged(EventArgs e)
 		{
 			if (WindowState == WindowState.Minimized)
 				this.Hide();
-
 			base.OnStateChanged(e);
+		}
+
+		private void Menu_Refresh(object sender, RoutedEventArgs e)
+		{
+			this.appWeather.Refresh();
+			testText.Text = appWeather.XMLResponse.City + " " + appWeather.XMLResponse.ConditionTemp + "C " + appWeather.XMLResponse.ConditionText + " " + appWeather.XMLResponse.Country;
+		}
+
+		private void Menu_Open(object sender, RoutedEventArgs e)
+		{
+			this.Show();
+			if (this.WindowState == WindowState.Minimized)
+				this.WindowState = WindowState.Normal;
+		}
+
+		private void Menu_Close(object sender, RoutedEventArgs e)
+		{
+			this.Close();
 		}
 	}
 }
